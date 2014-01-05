aefd9dad7c0853241faffef9816b5086c688607b
Jay Wang <jayduke15@gmail.com>
2014-01-04 23:07:24 -0800

    Fixed Bug and Feature Enhancement
    - Placed sManager register into onResume as opposed to onCreate
    - Unregister the sManager when activity leaves in onPause
    - Creation of object[] as parameter for altCntrlActivity
    - Before, only Activity objects could invoke methods
    - Allows for any type of object to invoke a method

--- altCntrlActivity.java
+++ altCntrlActivity.java
@@ -2,95 +2,93 @@ package altCntrl;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public abstract class altCntrlActivity extends Activity implements
 		SensorEventListener {
 
+	private double[] THRESHOLDS = {30.0, -30.0};
+	private double NOISE = 5.0;
 	public SensorManager sManager;
 	private boolean on = false;
 	private Method[] methods;
-	private Activity activity;
+	private Object[] objects;
 	private View view;
 	public double pitch;
 	
-	@SuppressWarnings("deprecation")
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		pitch = 0.0;
+	}
+	
+	public void altCntrlSetUp(Method[] methods, Object[] objects, View view) {
+		this.methods = methods;
+		this.objects = objects;
+		this.view = view;
+	}
+
+	@SuppressWarnings("deprecation")
+	public void onResume() {
+		super.onResume();
 		sManager.registerListener(this,
 				sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
 				SensorManager.SENSOR_DELAY_FASTEST);
 	}
 	
-	public void altCntrlSetUp(Method[] methods, Activity activity, View view) {
-		this.methods = methods;
-		this.activity = activity;
-		this.view = view;
+	public void onPause() {
+		super.onPause();
+		sManager.unregisterListener(this);
 	}
-
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		// if sensor is unreliable, return void
-		
-		
+				
 		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
 			return;
 		}
+		
 		pitch = event.values[1];
 	
-		if (Math.abs(event.values[1]) < 5.0) {
+		if (Math.abs(event.values[1]) < NOISE) {
 			on = true;
 		}
 					
-		if (pitch > 30.0) {
-			Log.i("Called. Pitch: ", Float.toString(event.values[1]));
+		if (pitch > THRESHOLDS[0]) {
 			event.values[1] = 0;
-			performAction(methods[0]);
-		} else if (pitch < -30.0) {
+			performAction(methods[0], objects[0]);
+		} else if (pitch < THRESHOLDS[1]) {
 			event.values[1] = 0;
-			performAction(methods[1]);
+			performAction(methods[1], objects[1]);
 		}
 
 	}
 	
-	public void performAction(Method action) {
+	public void performAction(Method action, Object object) {
 		
-		//Case when action was not set to anything 
 		if (action == null) return;
 		
 		if (on) {
 			try {
-				action.invoke(activity, view);
-//				sManager.unregisterListener(this);
+				action.invoke(object, view);
 				on = false;
 			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-
 }
